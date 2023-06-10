b326e8ebd71afd770c74313a536b08794faa3fa5
Kyz901 <kyz9.01.01@gmail.com>
2023-06-10 04:21:47 +0300

    init

---
+++ client/src/App.vue
@@ -0,0 +1,104 @@
+<template>
+ <div>
+  <h1>Test for Z2A</h1>
+   <hr>
+   <ul>
+     <li v-for="(data, i) in savedString" :key="i">
+       {{data}}
+     </li>
+   </ul>
+
+   <button @click="decrementPage">Left</button>
+   <button @click="incrementPage">Right</button>
+
+   <hr>
+   <div>
+    <label>Current page: {{this.pageNum}}</label>
+   </div>
+ </div>
+
+</template>
+
+<script>
+
+import axios from "axios";
+
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      jwtToken: generateJWT(),
+      savedString: [],
+      pageNum: 1,
+      baseUrl: 'http://localhost:8082/v1'
+    }
+  },
+
+  created() {
+    this.getData(this.pageNum, this.jwtToken)
+        .then(response => {
+          this.savedString = response;
+        })
+        .catch(error => {
+          console.error('Error fetch data:', error);
+        });
+  },
+
+  methods: {
+    decrementPage() {
+      if (this.pageNum > 1) {
+        this.pageNum--;
+
+        this.getData(this.pageNum, this.jwtToken)
+            .then(response => {
+              this.savedString = response;
+            })
+            .catch(error => {
+              console.error('Error fetch data:', error);
+            });
+      }
+    },
+
+    incrementPage() {
+      this.getData(this.pageNum+1, this.jwtToken)
+          .then(response => {
+            if(response.length) {
+              this.savedString = response;
+              this.pageNum++;
+            }
+          })
+          .catch(error => {
+            console.error('Error fetch data:', error);
+          });
+    },
+
+    getData(pageNum, token) {
+      return axios.get(`${this.baseUrl}/redis?page=${pageNum}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then((response) => {
+
+        return response.data.data;
+      }).catch((error) => {
+        console.error(error);
+      });
+    }
+  },
+}
+
+export const generateJWT = () => {
+  // const header = { "alg": "HS256", "typ": "JWT" };
+  // const payload = { "user": "John Doe" };
+  // const secretKey = "mcb3DaS05Yvt";
+
+  // return jwt.sign(header, payload, secretKey, { algorithm: 'HS256' });
+  return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiSm9obiBEb2UifQ.V5pM3HLRiP7eUEm99Tk54Sk6mTPpxw5YTqmI5IqOZOY';
+};
+
+</script>
+
+<style>
+
+</style>
---
+++ client/vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  // configureWebpack: {
+  //   resolve: {
+  //     fallback: {
+  //       "stream": require.resolve("stream-browserify"),
+  //       "util": require.resolve("util"),
+  //     },
+  //   },
+  // },
+})
